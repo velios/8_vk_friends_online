45017fb8bafff8c61165641606d3e296bf036a73
velios <prostozapomnit@gmail.com>
2017-08-15 17:15:12 +0000

    Major changes to program structure
    Merge input login and password
    Use getpass to hide password symbols
    Remove auth from file

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Watcher of Friends Online
 
-[TODO. There will be project description]
+The script displays a list of online friends from the most popular social network in Russia [VK.com](https://vk.com)
 
 # How to Install
 
@@ -8,6 +8,28 @@ Python 3 should be already installed. Then use pip (or pip3 if there is a confli
 
 ```bash
 pip install -r requirements.txt # alternatively try pip3
+```
+
+```bash
+# Run without arguments for input from keyboard mode
+$ python vk_friends_online.py
+Введите логин Вконтакте: ***
+Введите пароль Вконтакте:: ***
+# or from comandline with arguments
+$ python vk_friends_online.py --login *** --password ***
+# in this mode script create auth.txt JSON file with auth data
+# or load auth data from JSON data
+$ python vk_friends_online.py --config filepath
+```
+
+### Output:
+```bash
+В данный момент N ваших друзей online..
+
+1   Name     Surname            https://vk.com/id******          Mobile
+2   Name2    Surname2           https://vk.com/id******          Mobile
+3   Name3    Surname3           https://vk.com/id******          Mobile
+N   NameN    SurnameN           https://vk.com/id******          Desktop
 ```
 
 Remember, it is recommended to use [virtualenv/venv](https://devman.org/encyclopedia/pip/pip_virtualenv/) for better isolation.
--- vk_friends_online.py
+++ vk_friends_online.py
@@ -1,32 +1,64 @@
 import vk
+import argparse
+import logging
+import getpass
 
 
-APP_ID = -1  # чтобы получить app_id, нужно зарегистрировать своё приложение на https://vk.com/dev
+APP_ID = 6146727
+DEFAULT_AUTH_FILE_PATH = 'auth.txt'
+VERBOSITY_TO_LOGGING_LEVELS = {
+    0: logging.WARNING,
+    1: logging.INFO,
+    2: logging.DEBUG,
+}
 
 
-def get_user_login():
-    pass
+def get_user_login_and_password_from_cmd():
+    user_login = input('Введите логин: ')
+    user_password = getpass.getpass('Введите пароль: ')
+    return user_login, user_password
 
 
-def get_user_password():
-    pass
-
-
-def get_online_friends(login, password):
+def get_online_vk_friends(vk_login, vk_password, scope=None):
     session = vk.AuthSession(
         app_id=APP_ID,
-        user_login=login,
-        user_password=password,
+        user_login=vk_login,
+        user_password=vk_password,
+        scope=scope
     )
-    api = vk.API(session)
-    # например, api.friends.get()
+    vk_api = vk.API(session)
+    online_friends_ids_list = vk_api.friends.getOnline()
+    online_friends_list = vk_api.users.get(user_ids=online_friends_ids_list, fields='online')
+    return online_friends_list
 
 
-def output_friends_to_console(friends_online):
-    pass
+def output_friends_to_console(online_friends_list):
+    print('В данный момент {} ваших друзей online.\n'.format(len(online_friends_list)))
+    for index, friend_data in enumerate(online_friends_list):
+        if 'online_mobile' in friend_data:
+            friend_from_mobile_text = 'Mobile'
+        else:
+            friend_from_mobile_text = 'Desktop'
+        print('{4:<3} {0:10} {1:20} https://vk.com/id{2:<15} {3}'.format(friend_data['first_name'], friend_data['last_name'],
+                                                                         friend_data['uid'], friend_from_mobile_text, index + 1))
+    print('\n')
+
+
+def configurate_cmd_parser():
+    parser_description = ("\nСкрипт показывает кто из ваших друзей вконтакте онлайн в данный момент.\n")
+    cmd_arguments = argparse.ArgumentParser(description=parser_description)
+    cmd_arguments.add_argument('--verbose', '-v', action='count', default=0)
+    return cmd_arguments.parse_args()
+
 
 if __name__ == '__main__':
-    login = get_user_login()
-    password = get_user_password()
-    friends_online = get_online_friends(login, password)
-    output_friends_to_console(friends_online)
+    cmd_arguments = configurate_cmd_parser()
+    logging_level = VERBOSITY_TO_LOGGING_LEVELS[cmd_arguments.verbose]
+    logging.basicConfig(level=logging_level)
+
+    vk_login, vk_password = get_user_login_and_password_from_cmd()
+    try:
+        friends_online = get_online_vk_friends(vk_login, vk_password, scope='friends, users')
+        output_friends_to_console(friends_online)
+    except Exception:
+        print('Ошибка авторизации')
