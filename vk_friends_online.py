054f6c3191b8b7f0a855e9b96a14343e6dbc0859
velios <prostozapomnit@gmail.com>
2017-08-22 23:11:40 +0300

    Made minor usability changes
    Full compliance with PEP8
    Add header when program run

--- vk_friends_online.py
+++ vk_friends_online.py
@@ -3,7 +3,6 @@ import argparse
 import logging
 import getpass
 
-
 APP_ID = 6146727
 DEFAULT_AUTH_FILE_PATH = 'auth.txt'
 VERBOSITY_TO_LOGGING_LEVELS = {
@@ -14,6 +13,8 @@ VERBOSITY_TO_LOGGING_LEVELS = {
 
 
 def get_user_login_and_password_from_cmd():
+    print('Программа показывает кто из ваших друзей '
+          'Вконтакте онлайн в данный момент')
     user_login = input('Введите логин: ')
     user_password = getpass.getpass('Введите пароль: ')
     return user_login, user_password
@@ -28,24 +29,31 @@ def get_online_vk_friends(vk_login, vk_password, scope=None):
     )
     vk_api = vk.API(session)
     online_friends_ids_list = vk_api.friends.getOnline()
-    online_friends_list = vk_api.users.get(user_ids=online_friends_ids_list, fields='online')
+    online_friends_list = vk_api.users.get(user_ids=online_friends_ids_list,
+                                           fields='online')
     return online_friends_list
 
 
 def output_friends_to_console(online_friends_list):
-    print('В данный момент {} ваших друзей online.\n'.format(len(online_friends_list)))
+    print('В данный момент {} ваших друзей '
+          'online.\n'.format(len(online_friends_list)))
     for index, friend_data in enumerate(online_friends_list):
         if 'online_mobile' in friend_data:
             friend_from_mobile_text = 'Mobile'
         else:
             friend_from_mobile_text = 'Desktop'
-        print('{4:<3} {0:10} {1:20} https://vk.com/id{2:<15} {3}'.format(friend_data['first_name'], friend_data['last_name'],
-                                                                         friend_data['uid'], friend_from_mobile_text, index + 1))
+        print('{4:<3} {0:10} {1:20} '
+              'https://vk.com/id{2:<15} {3}'.format(friend_data['first_name'],
+                                                    friend_data['last_name'],
+                                                    friend_data['uid'],
+                                                    friend_from_mobile_text,
+                                                    index + 1))
     print('\n')
 
 
 def configurate_cmd_parser():
-    parser_description = ("\nСкрипт показывает кто из ваших друзей вконтакте онлайн в данный момент.\n")
+    parser_description = ('\nСкрипт показывает кто из ваших друзей '
+                          'вконтакте онлайн в данный момент.\n')
     cmd_arguments = argparse.ArgumentParser(description=parser_description)
     cmd_arguments.add_argument('--verbose', '-v', action='count', default=0)
     return cmd_arguments.parse_args()
@@ -58,9 +66,9 @@ if __name__ == '__main__':
 
     vk_login, vk_password = get_user_login_and_password_from_cmd()
     try:
-        friends_online = get_online_vk_friends(vk_login, vk_password, scope='friends, users')
+        friends_online = get_online_vk_friends(vk_login,
+                                               vk_password,
+                                               scope='friends, users')
         output_friends_to_console(friends_online)
     except vk.exceptions.VkAuthError:
         print('Ошибка авторизации')
-    else:
-        print('Неизвестная ошибка')
